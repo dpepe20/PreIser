2f1613f9dda05de6d327fa6c67e60062c4e79ffd
dpepe20 <44669597+dpepe20@users.noreply.github.com>
2018-12-07 09:34:35 -0500

    Update forms.py

--- home/forms.py
+++ home/forms.py
@@ -84,7 +84,7 @@ class agregar_prestamoF(forms.ModelForm):
 
 	class Meta:
 		model=Prestamo
-		exculde =['estado']
+		exclude =['estado']
 		fields='__all__'
 		widgets = {
 		'fecha_prestamo':forms.TextInput(attrs={
