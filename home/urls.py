00e5905359fd895d1b9b8089f3fe0181a3956b5c
dpepe20 <44669597+dpepe20@users.noreply.github.com>
2018-12-10 10:26:24 -0500

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -44,6 +44,7 @@ urlpatterns =[
 	path('editar_material/<int:id_material>/',vista_editar_material,name="vista_editar_material"),
 	path('eliminar_material/<int:id_material>/',vista_eliminar_material,name="vista_eliminar_material"),
 	path('ver_material/<int:id_material>/',vista_ver_material,name="vista_ver_material"),
+	path('materiales_no_disponibles',vista_materiales_no_disponibles,name="vista_materiales_no_disponiblesl"),
 #----------------------------------------------------------------------------------------------------------
 #urls de devoluciones
 	path('devolucion/<int:id_prest>/',devolucion_prestamo,name='devolucion_prestamo'),
@@ -103,6 +104,7 @@ urlpatterns =[
 	path('reporte/',vista_reporte,name="vista_reporte"),
 	path('materiales_disponibles/',vista_materiales_disponibles,name="vista_materiales_disponibles"),
 	path('reporte_material/',vista_reporte_material,name="vista_reporte_material")
+	path('reportes/',vista_reportes,name='vista_reportes'),
 	
 	
 ]
